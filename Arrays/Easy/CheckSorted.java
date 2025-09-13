593c9e417d0b6f7afa6fcd1f6dba25de5cc3f0d1
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-09-13 10:45:15 +0530

    check array is sorted

---
+++ Arrays/Easy/CheckSorted.java
@@ -0,0 +1,17 @@
+package Arrays.Easy;
+
+public class CheckSorted {
+    static boolean isSorted(int arr[], int n) {
+        for(int i=1; i<n; i++) {
+            if(arr[i] < arr[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        System.out.println(isSorted(arr, n));
+    }
+}
