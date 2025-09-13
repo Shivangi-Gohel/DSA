55ef7d3a2cf2a2dd746dd23f9754acbe67483e73
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-09-13 21:30:05 +0530

    remove duplicates from array

---
+++ Arrays/Easy/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package Arrays.Easy;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 3, 3, 4, 5, 2};
+        int i = 0;
+        for(int j=1; j<arr.length; j++) {
+            if(arr[i] != arr[j]) {
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        System.out.println(i+1);
+        for(int k=0; k<=i; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
