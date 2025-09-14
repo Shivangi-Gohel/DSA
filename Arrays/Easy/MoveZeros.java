8c06de7ddafb847f87da4107ee6cf32ecdb6a825
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-09-14 14:51:22 +0530

    move zeros to end of the array

--- Arrays/Easy/MoveZeros.java
+++ Arrays/Easy/MoveZeros.java
@@ -1,1 +1,27 @@
+package Arrays.Easy;
 
+public class MoveZeros {
+    static public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while(i<n) {
+            if(nums[i] == 0) {
+                for(int j = i; j<n-1; j++) {
+                    nums[j] = nums[j+1];
+                }
+                nums[n-1] = 0;
+                n--;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {0,1,0,3,12};
+        moveZeroes(arr);
+        for(int i: arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
